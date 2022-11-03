c6e5b7e4b8524db4634a427d3d26abe69d77e0ec
Stanisław Rogasik <90356612+stanislawrogasik@users.noreply.github.com>
2022-11-03 16:42:30 +0100

    Add files via upload

--- beta/js/settings.js
+++ beta/js/settings.js
@@ -1,15 +1,32 @@
 console.log('Settings opened')
 
 function init(){
+    if(localStorage.localCredentials==undefined){
+        console.log("Creating local credentials inside local storage as it do not exist!")
+        localStorage.localCredentials=JSON.stringify([])
+    }
     //$("#baseURL")[0].value=localStorage.baseUrl
     //$("#apiKey")[0].value=localStorage.apiKey
     //$("#name")[0].value=localStorage.name
 }
 
 function saveSettings(){
-    localStorage.baseUrl=$("#baseURL")[0].value
-    localStorage.apiKey=$("#apiKey")[0].value
-    localStorage.name=$("#name")[0].value
+    let localCredentials=JSON.parse(localStorage.localCredentials)
+    //{"system":"","baseURL":"","apiKey":"","userID":""}
+    let sysSelected = $("#systemSelect")
+    let selectedSystemName = sysSelected[0].selectedOptions[0].text
+    if(localCredentials.filter(x=>x.system==selectedSystemName).length==0){
+        localCredentials.push({"system":selectedSystemName,"baseURL":"","apiKey":"","userID":""})
+    }
+    for(i of localCredentials){
+        if(i.system==selectedSystemName){
+            i.baseURL=$("#baseURL")[0].value
+            i.apiKey=$("#apiKey")[0].value
+            i.userID=$("#name")[0].value
+        }
+    }
+    localStorage.localCredentials=JSON.stringify(localCredentials)
+
 }
 
 function hideUnhideHelp() {
