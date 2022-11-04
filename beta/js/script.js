7a21bc2dc0feefd5e0034cd92160d4af8b7a085a
Stanisław Rogasik <90356612+stanislawrogasik@users.noreply.github.com>
2022-11-04 01:29:23 +0100

    Almost all changes

--- beta/js/script.js
+++ beta/js/script.js
@@ -1,22 +1,61 @@
 console.log('App started')
 
-let apikey = ""
-let name = ""
 let currentTime = (new Date().getTime())
-let totalTimeLog = 0;
-let timelogArray=[]
-let currTableIndex=0;
-let baseUrl=""
+let timelogArray = []
+let currTableIndex = 0;
+let totalTimeLog =  parseInt(localStorage.totalTimeLog)
 
-
-async function addWorklogV3(id, description, workminutes, portalid) {
-    let url = baseUrl+"api/v3/worklog?TECHNICIAN_KEY=" + apikey;
-    let jsonStr=""
-    if(parseInt(name) || 0){
-        jsonStr={"id":name.toString()}
+//this is function to add new systems based on currently available fields:
+//you need to create proper function for your system and update "addGeneralWorklog"
+///requestID - self explanatory
+///description - self explanatory
+///tempWorkTime - time in minutes 
+///credentials - JSON object
+//// system - system name
+//// baseURL - URL of system with "/" at the end!
+//// apiKey - API key used to authenticate against the API
+//// userID - identification of user, can be name or ID
+/// endTime - time in which the timelog was sent
+function addGeneralWorklog(requestID, description, tempWorktime, credentials, endTime){
+    if(credentials.system=="SupportCenter"){
+    addWorklogV3SupportCenter(requestID, description, tempWorktime, getPortalID(requestID),credentials.userID, credentials.baseURL,credentials.apiKey).then((val) => {
+        console.log(val);
+        if (val['response_status']['status'] != undefined && val['response_status']['status'] == "success") {
+            ($("#worklogTable")[0].insertRow(1)).innerHTML = getRowHTML(++currTableIndex, requestID, description, tempWorktime.toString() + " mins", new Date(endTime).toLocaleString(), true,credentials.system)
+            timelogArray.push({ "nr": currTableIndex, "id": requestID, "desc": description, "time": tempWorktime, "date": new Date(endTime).toLocaleString(), "status": true, "system":credentials.system })
+        }
+        else {
+            ($("#worklogTable")[0].insertRow(1)).innerHTML = getRowHTML(++currTableIndex, requestID, description, tempWorktime.toString() + " mins", new Date(endTime).toLocaleString(), false,credentials.system)
+            timelogArray.push({ "nr": currTableIndex, "id": requestID, "desc": description, "time": tempWorktime, "date": new Date(endTime).toLocaleString(), "status": false, "system":credentials.system })
+        }
+        localStorage.timelogArray = JSON.stringify(timelogArray)
+        }
+        )
     }
-    else{
-        jsonStr={"name":name}
+    else if(credentials.system="ServiceDesk"){
+        addWorklogV3ServiceDesk(requestID, description, tempWorktime, getPortalID(requestID),credentials.userID, credentials.baseURL,credentials.apiKey).then((val) => {
+            ($("#worklogTable")[0].insertRow(1)).innerHTML = getRowHTML(++currTableIndex, requestID, description, tempWorktime.toString() + " mins", new Date(endTime).toLocaleString(), true,credentials.system)
+            timelogArray.push({ "nr": currTableIndex, "id": requestID, "desc": description, "time": tempWorktime, "date": new Date(endTime).toLocaleString(), "status": true, "system":credentials.system })
+            localStorage.timelogArray = JSON.stringify(timelogArray)
+            }).catch((error) => {
+                ($("#worklogTable")[0].insertRow(1)).innerHTML = getRowHTML(++currTableIndex, requestID, description, tempWorktime.toString() + " mins", new Date(endTime).toLocaleString(), true,credentials.system)
+                timelogArray.push({ "nr": currTableIndex, "id": requestID, "desc": description, "time": tempWorktime, "date": new Date(endTime).toLocaleString(), "status": false, "system":credentials.system })
+                 localStorage.timelogArray = JSON.stringify(timelogArray)
+                console.log("Network error - but adding time!")
+              })
+        }
+    }
+
+
+//compatible with SupportCenter
+async function addWorklogV3SupportCenter(id, description, workminutes, portalid, userID, baseURL, apiKey) {
+    let url = baseURL + "api/v3/worklog?TECHNICIAN_KEY=" + apiKey;
+    let jsonStr = ""
+    if (parseInt(userID) || 0) {
+        jsonStr = { "id": userID.toString() }
+    }
+    else {
+        jsonStr = { "name": userID }
     }
 
     // Default options are marked with *
@@ -25,14 +64,14 @@ async function addWorklogV3(id, description, workminutes, portalid) {
         headers: {
             'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'
         },
-        body: "TECHNICIAN_KEY="+apikey+"&format=json&portalid="+portalid+"&INPUT_DATA=" + JSON.stringify({
+        body: "TECHNICIAN_KEY=" + apiKey + "&format=json&portalid=" + portalid + "&INPUT_DATA=" + JSON.stringify({
             "worklog": {
                 "request": {
                     "id": id.toString()
                 },
                 "description": description,
                 "technician": jsonStr,
-                "total_time_spent": (workminutes*60*1000).toString()
+                "total_time_spent": (workminutes * 60 * 1000).toString()
             }
         })
 
@@ -40,14 +79,47 @@ async function addWorklogV3(id, description, workminutes, portalid) {
     return response.json(); // parses JSON response into native JavaScript objects
 }
 
-async function addWorklogV1(id, description, workminutes, portalid) {
-    let url = baseUrl+"sdpapi/request/" + id + "/worklog";
+async function addWorklogV3ServiceDesk(id, description, workminutes, portalid, userID, baseURL, apiKey) {
+    let url = baseURL + "api/v3/worklog?TECHNICIAN_KEY=" + apiKey;
+    let jsonStr = ""
+    if (parseInt(userID) || 0) {
+        jsonStr = { "id": userID.toString() }
+    }
+    else {
+        jsonStr = { "name": userID }
+    }
+
+    // Default options are marked with *
     const response = await fetch(url, {
         method: 'POST', // *GET, POST, PUT, DELETE, etc.
         headers: {
             'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'
         },
-        body: "format=json&TECHNICIAN_KEY="+apikey+"&portalid=" + portalid + "&INPUT_DATA=" + JSON.stringify({
+        body: "input_data=" + JSON.stringify({
+            "worklog": {
+                "request": {
+                    "id": id.toString()
+                },
+                "description": description,
+                "technician": jsonStr,
+                "total_time_spent": (workminutes * 60 * 1000).toString()
+            }
+        })
+
+    });
+    return response.json(); // parses JSON response into native JavaScript objects
+}
+
+//unused function - leaving for history 
+/*
+async function addWorklogV1(id, description, workminutes, portalid) {
+    let url = baseUrl + "sdpapi/request/" + id + "/worklog";
+    const response = await fetch(url, {
+        method: 'POST', // *GET, POST, PUT, DELETE, etc.
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'
+        },
+        body: "format=json&TECHNICIAN_KEY=" + apikey + "&portalid=" + portalid + "&INPUT_DATA=" + JSON.stringify({
             "operation": {
                 "details": {
                     "worklogs": {
@@ -64,20 +136,21 @@ async function addWorklogV1(id, description, workminutes, portalid) {
     });
     return response.json(); // parses JSON response into native JavaScript objects
 }
+*/
 
-
-function getRowHTML(index, requestid, description, timetaken, date,status) {
-    if(status==true){
-        return '<th scope="row" style="background:lightgreen">' + index.toString() + '</th><td>' + requestid.toString() + '</td><td>' + description.toString() + '</td><td>' + timetaken.toString() + '</td><td>' + date + '</td>'
+function getRowHTML(index, requestid, description, timetaken, date, status, system) {
+    if (status == true) {
+        return '<th scope="row" style="background:#008000">' + index.toString() + '</th><td>' + requestid.toString() + '</td><td>' + description.toString() + '</td><td>' + timetaken.toString() + '</td><td>' + date + '</td><td>'+system+"</td>"
     }
-    else if(status==false) {
-        return '<th scope="row" style="background:orangered">' + index.toString() + '</th><td>' + requestid.toString() + '</td><td>' + description.toString() + '</td><td>' + timetaken.toString() + '</td><td>' + date + '</td>'
+    else if (status == false) {
+        return '<th scope="row" style="background:orangered">' + index.toString() + '</th><td>' + requestid.toString() + '</td><td>' + description.toString() + '</td><td>' + timetaken.toString() + '</td><td>' + date + '</td><td>'+system+"</td>"
     }
-    else{
-        return '<th scope="row">' + index.toString() + '</th><td>' + requestid.toString() + '</td><td>' + description.toString() + '</td><td>' + timetaken.toString() + '</td><td>' + date + '</td>'
+    else {
+        return '<th scope="row">' + index.toString() + '</th><td>' + requestid.toString() + '</td><td>' + description.toString() + '</td><td>' + timetaken.toString() + '</td><td>' + date + '</td><td>SupportCenter</td>'
     }
 }
 
+//we need to hide table based on button
 function hideUnhideTable() {
     let table = $("#worklogTable")[0]
     if (table.classList.contains("d-none")) {
@@ -88,120 +161,131 @@ function hideUnhideTable() {
 }
 
 function sendTime() {
-    let requestid = $("#requestid")[0].value
-    let status = ''
+    let requestID = $("#requestid")[0].value
     let description = $("#description")[0].value
     let endTime = (new Date().getTime())
+    //manipulate time
     let manTime = $("#manTime")[0].value
-    //getting minutes
+    //getting minutes rounded to higher value
     let tempWorktime = Math.ceil((endTime - currentTime) / (1000 * 60)) + parseInt(manTime)
+    //calculating totalTimeLog
     totalTimeLog += tempWorktime
-    addWorklogV3(requestid, description, tempWorktime, getPortalID(requestid)).then((val)=>
-    {console.log(val);
-        if(val['response_status']['status']!=undefined&&val['response_status']['status']=="success"){
-        ($("#worklogTable")[0].insertRow(1)).innerHTML = getRowHTML(++currTableIndex, requestid, description, tempWorktime.toString()+" mins",new Date(endTime).toLocaleString(),true)
-         timelogArray.push({"nr":currTableIndex,"id":requestid,"desc":description,"time":tempWorktime,"date":new Date(endTime).toLocaleString(),"status":true})
-    }
-    else{
-        ($("#worklogTable")[0].insertRow(1)).innerHTML = getRowHTML(++currTableIndex, requestid, description, tempWorktime.toString()+" mins",new Date(endTime).toLocaleString(),false)
-         timelogArray.push({"nr":currTableIndex,"id":requestid,"desc":description,"time":tempWorktime,"date":new Date(endTime).toLocaleString(),"status":false})
-    }
-        localStorage.timelogArray=JSON.stringify(timelogArray)
 
+    //get current credentials to push data(in case somebody changed the settings)
+    //object localCredentialsJSON exists due to "loadSystemsIntoMainPage" function
+    let systemDropdownObj = $("#selectedSystemDropdown")
+    let selectedSystemText = systemDropdownObj[0].selectedOptions[0].text
+    let selectedCreds = localCredentialsJSON.filter(x=>x.system==selectedSystemText)[0]
+    
+    //debugging selected creds!
+    console.log("Selected credentials!")
+    console.log(selectedCreds)
 
-    }
-    )
-    /* //FOR V1
-
-     addWorklogV1(requestid, description, tempWorktime, getPortalID(requestid)).then((val)=>
-    {if(val['operation']['result']['status']!=undefined&&val['operation']['result']['status']=="Success"){
-        ($("#worklogTable")[0].insertRow(1)).innerHTML = getRowHTML(++currTableIndex, requestid, description, tempWorktime.toString()+" mins",new Date(endTime).toLocaleString(),true)
-         timelogArray.push({"nr":currTableIndex,"id":requestid,"desc":description,"time":tempWorktime,"date":new Date(endTime).toLocaleString(),"status":true})
-    }
-    else{
-        ($("#worklogTable")[0].insertRow(1)).innerHTML = getRowHTML(++currTableIndex, requestid, description, tempWorktime.toString()+" mins",new Date(endTime).toLocaleString(),false)
-         timelogArray.push({"nr":currTableIndex,"id":requestid,"desc":description,"time":tempWorktime,"date":new Date(endTime).toLocaleString(),"status":false})
-    }
-        localStorage.timelogArray=JSON.stringify(timelogArray)
-    }
-    )
-
-     */
-    //addWorklogV3(requestid, description, currentTime, (endTime+parseInt(manTime)*1000*60), getPortalID(requestid))
-    //////we're past sending data
+    //asynchronous function to add worklog
+    //todo - try catch around add worklog
+    addGeneralWorklog(requestID, description, tempWorktime, selectedCreds, endTime)
+    //we need to have new "beggining" time instead of previous one
     currentTime = endTime
-    //setting current time
+
+    //in case if we will close the webpage - we're saving the data into local storage
     $("#currentTime")[0].value = "Current time: " + (new Date(endTime).toLocaleString())
     $("#totalTimelog")[0].value = "Total timelog: " + totalTimeLog.toString() + " mins";
-    localStorage.lastEndTime=endTime
-    $("#requestid")[0].value=""
-    $("#description")[0].value=""
+    localStorage.lastEndTime = endTime
+    localStorage.totalTimeLog = totalTimeLog
+    //we're clearing the inputs after sending time and getting back response
+    $("#requestid")[0].value = ""
+    $("#description")[0].value = ""
 }
 
-function getPortalID(id){
+//this functtion can be modified in case of unsupported SupportCenter
+function getPortalID(id) {
+    //this is used for SupportCenter and portal IDs - change if neccessarry!
     let ret = 0;
-    switch(true){
-        case(id<100000000): return 1;
-        case(id<800000000): return 301;
-        case(id>800000000): return 907;
+    switch (true) {
+        case (id < 100000000): return 1;
+        case (id < 800000000): return 301;
+        case (id > 800000000): return 907;
     }
 }
 
-function clearWorklog(){
-    let confirm=window.confirm("Are you sure to clear worklog?");
-    if(confirm){
+function clearWorklog() {
+    let confirm = window.confirm("Are you sure to clear worklog?");
+    if (confirm) {
         console.log("SURE!")
-        localStorage.timelogArray=[]
+        localStorage.timelogArray = []
         timelogArray = []
-        let table=$("#worklogTable")[0]
-        while(table.rows.length!=1){
+        let table = $("#worklogTable")[0]
+        while (table.rows.length != 1) {
             table.deleteRow(1)
         }
-        currTableIndex=0;
+        currTableIndex = 0;
     }
 
 }
 
-function resetTime(){
-    let confirm=window.confirm("Are you sure that you want to reset time?");
-    if(confirm){
+function resetTime() {
+    let confirm = window.confirm("Are you sure that you want to reset time?");
+    if (confirm) {
         let endTime = (new Date().getTime())
         currentTime = endTime
         $("#currentTime")[0].value = "Current time: " + (new Date(endTime).toLocaleString())
-        localStorage.lastEndTime=endTime
+        localStorage.lastEndTime = endTime
     }
 }
 
-function init(){
-    $("#manTime")[0].value=1
-    if(localStorage.debug!=undefined){
+function loadSystemsIntoMainPage(){
+    let objPointer = $("#selectedSystemDropdown")[0]
+    localCredentialsJSON = JSON.parse(localStorage.localCredentials)
+    for(i of localCredentialsJSON){
+        objPointer.add(new Option(i.system))
+    }
+    if(localCredentialsJSON.length==0){
+        objPointer.add(new Option("Configure timer - in settings!"))
+        //alert("You didn't configure the local credentials - please head up to settings!")
+    }
+}
+
+function init() {
+    //if we don't have a proper object!
+    if(localStorage.localCredentials==undefined){
+        console.log("Creating empty local creds")
+        localStorage.localCredentials=JSON.stringify([])
+    }
+    //default adding minutes
+    $("#manTime")[0].value = 1
+    //if debugMode is enabled!
+    if (localStorage.debug != undefined) {
         $("#manTime")[0].classList.remove("d-none")
     }
-    apikey = localStorage.apiKey
-    name = localStorage.name
-    baseUrl = localStorage.baseUrl
-    lastEndTime=localStorage.lastEndTime
+    //loading systems from the localStorage into the HTML
+    loadSystemsIntoMainPage()
+
+    //in case somebody did exit from a webpage
+    lastEndTime = localStorage.lastEndTime
+    $("#totalTimelog")[0].value = "Total timelog: " + localStorage.totalTimeLog.toString() + " mins";
     //just to check
-    console.log("Local storage loaded. Name: '"+name +"', Base URL: '"+baseUrl+"', APIKEY: '"+apikey+"'")
+    console.log("Local credentials: "+localStorage.localCredentials)
 
     //if last endTime is today, then set-it
-    if((new Date(lastEndTime/1).toDateString())==(new Date().toDateString())){
+    if ((new Date(lastEndTime / 1).toDateString()) == (new Date().toDateString())) {
         console.log("Last end time is today! Restoring proper date!")
         currentTime = lastEndTime
+        $("#totalTimelog")[0].value = "Total timelog: "+parseInt(localStorage.totalTimeLog)+" mins";
     }
-    $("#currentTime")[0].value = "Current time: " + (new Date(currentTime/1).toLocaleString())
+    $("#currentTime")[0].value = "Current time: " + (new Date(currentTime / 1).toLocaleString())
     //getting data from local storage
+
     try {
         timelogArray = JSON.parse(localStorage.timelogArray)
     } catch (error) {
-        timelogArray=[]
+        timelogArray = []
     }
     //putting previous time requests
-    if(timelogArray.length>0){
-            for(entry of timelogArray){
-            ($("#worklogTable")[0].insertRow(1)).innerHTML = getRowHTML(entry.nr, entry.id, entry.desc, entry.time.toString()+" mins",entry.date,entry.status)
-            }
-        currTableIndex=timelogArray[timelogArray.length-1].nr
+    if (timelogArray.length > 0) {
+        for (entry of timelogArray) {
+            ($("#worklogTable")[0].insertRow(1)).innerHTML = getRowHTML(entry.nr, entry.id, entry.desc, entry.time.toString() + " mins", entry.date, entry.status, entry.system)
+        }
+        currTableIndex = timelogArray[timelogArray.length - 1].nr
     }
     //setting current index for table
 
--- beta/js/settings.js
+++ beta/js/settings.js
@@ -1,32 +1,54 @@
 console.log('Settings opened')
 
-function init(){
-    if(localStorage.localCredentials==undefined){
+function init() {
+    //new version
+    if (localStorage.localCredentialsJSON == undefined) {
         console.log("Creating local credentials inside local storage as it do not exist!")
-        localStorage.localCredentials=JSON.stringify([])
+        console.log("Moving SupportCenter to proper JSON format!")
+        localStorage.localCredentialsJSON = JSON.stringify([{ "system": "SupportCenter", "baseURL": localStorage.baseURL, "apiKey": localStorage.apiKey, "userID": localStorage.name }])
     }
+
+    //old version -- for historic purpose
     //$("#baseURL")[0].value=localStorage.baseUrl
     //$("#apiKey")[0].value=localStorage.apiKey
     //$("#name")[0].value=localStorage.name
 }
 
-function saveSettings(){
-    let localCredentials=JSON.parse(localStorage.localCredentials)
-    //{"system":"","baseURL":"","apiKey":"","userID":""}
-    let sysSelected = $("#systemSelect")
-    let selectedSystemName = sysSelected[0].selectedOptions[0].text
-    if(localCredentials.filter(x=>x.system==selectedSystemName).length==0){
-        localCredentials.push({"system":selectedSystemName,"baseURL":"","apiKey":"","userID":""})
+function saveSettings() {
+    //JSON with local credentials for all systems
+    let localCredentialsJSON = JSON.parse(localStorage.localCredentials)
+    let systemDropdownObj = $("#systemSelect")
+    let selectedSystemText = systemDropdownObj[0].selectedOptions[0].text
+    //in case there's no local credentials in local storage
+    if (localCredentialsJSON.filter(x => x.system == selectedSystemText).length == 0) {
+        localCredentialsJSON.push({ "system": selectedSystemText, "baseURL": "", "apiKey": "", "userID": "" })
     }
-    for(i of localCredentials){
-        if(i.system==selectedSystemName){
-            i.baseURL=$("#baseURL")[0].value
-            i.apiKey=$("#apiKey")[0].value
-            i.userID=$("#name")[0].value
+    for (i of localCredentialsJSON) {
+        if (i.system == selectedSystemText) {
+            i.baseURL = $("#baseURL")[0].value
+            i.apiKey = $("#apiKey")[0].value
+            i.userID = $("#name")[0].value
         }
     }
-    localStorage.localCredentials=JSON.stringify(localCredentials)
+    localStorage.localCredentials = JSON.stringify(localCredentialsJSON)
 
+}
+
+function loadDetailsAboutSystem(){
+    let systemDropdownObj = $("#systemSelect")
+    let selectedSystemText = systemDropdownObj[0].selectedOptions[0].text
+    let localCredentialsJSON = JSON.parse(localStorage.localCredentials)
+    let selectedData=localCredentialsJSON.filter(x => x.system == selectedSystemText)[0]
+    if(selectedData!=undefined){
+        $("#baseURL")[0].value=selectedData.baseURL
+        $("#apiKey")[0].value=selectedData.apiKey
+        $("#name")[0].value=selectedData.userID
+    }
+    else{
+        $("#baseURL")[0].value=""
+        $("#apiKey")[0].value=""
+        $("#name")[0].value=""
+    }
 }
 
 function hideUnhideHelp() {
@@ -39,25 +61,27 @@ function hideUnhideHelp() {
 }
 
 function systemSelected() {
-  let sysSelected = $("#systemSelect")
-  let settings = $("#settingsInputs")[0]
-  switch (sysSelected[0].selectedIndex) {
-    case 0:
-        
-        alert("Choose different than 'Select system'")
-        break;
-    //case when supportcenter
-    case 1:
-        settings.classList.remove("d-none")
-        break;
-    //case when servicedesk
-    case 2:
-        settings.classList.remove("d-none")
-        break;
-    default:
-        break;
-  }
-  console.log(sysSelected);
+    let systemDropdownObj = $("#systemSelect")
+    let settings = $("#settingsInputs")[0]
+    switch (systemDropdownObj[0].selectedIndex) {
+        case 0:
+            //alert("Choose different than 'Select system'")
+            settings.classList.add("d-none")
+            break;
+        //case when supportcenter
+        case 1:
+            settings.classList.remove("d-none")
+            loadDetailsAboutSystem()
+            break;
+        //case when servicedesk
+        case 2:
+            settings.classList.remove("d-none")
+            loadDetailsAboutSystem()
+            break;
+        default:
+            break;
+    }
+    console.log(systemDropdownObj);
 }
 
 $('#save')
@@ -65,6 +89,6 @@ $('#save')
 
 $('#activeHelpCards').on('click', () => hideUnhideHelp())
 
-$('#systemSelect').on('click',()=>systemSelected())
+$('#systemSelect').on('click', () => systemSelected())
 
 init()
